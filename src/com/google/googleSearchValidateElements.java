58c8e5f46bb105c1717aab062ceddc01f227bd31
Aglamh1 <aglamh1@gmail.com>
2021-11-10 21:48:13 -0600

    Second commit

--- src/com/google/googleSearchValidateElements.java
+++ src/com/google/googleSearchValidateElements.java
@@ -1,12 +1,19 @@
 package com.google;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import objectRepository.GoogleHomePage;
 
 public class googleSearchValidateElements {
 
 	private WebDriver driver;
+
 	
 	@BeforeTest
 	public void setUp() {
@@ -16,5 +23,20 @@ public class googleSearchValidateElements {
 		driver.manage().window().maximize();
 		driver.get("https://google.com");
 		
+	}
+	
+	@Test
+	public void googleSearchList() {
+		
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		GoogleHomePage homepage = new GoogleHomePage(driver);
+		//System.out.println(homepage.getClass()); searchBox
+		WebElement findElement(By searchBox);
+		
+	}
+	
+	@AfterTest
+	public void closeBrowser() {
+		driver.close();
 	}
 }
---
+++ src/objectRepository/GoogleHomePage.java
@@ -0,0 +1,28 @@
+package objectRepository;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class GoogleHomePage {
+	
+	
+	WebDriver driver;
+	public GoogleHomePage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	//PENDEJADA DEL WEY DE LOS VIDEOS
+	/*
+	public WebElements tag(String tag) {
+		return driver.findElements(tag);
+	} */
+	
+	public WebElement findElement(By locator) {
+		return driver.findElement(locator);
+	}
+	
+	By searchBox = By.name("q");
+	By googleLogo = By.xpath("//img[@alt='Google']");
+	
+}
